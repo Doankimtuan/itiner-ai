acee0e1ce3cd95eb06f3bd3ea2b1e62c4dad4269
Tuan Doan <Doantuan21101999@gmail.com>
2024-11-19 21:23:51 +0700

    [Feature] Install dependence

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirectOptions: {
+      login: "/",
+      callback: "/confirm",
+      exclude: ["/api/*"],
+    },
+  },
+});
